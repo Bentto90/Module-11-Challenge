e647bb16d93cc9a42a2e1879f00cf84c727c17e3
bentto90 <cch3056@gmail.com>
2023-03-16 20:14:18 -0700

    pushing

--- 02-Challenge/Develop/server.js
+++ 02-Challenge/Develop/server.js
@@ -1,14 +1,14 @@
+// importing express package
 const express = require('express');
 
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
 const app = express();
-
+// 
 const PORT = process.env.PORT || 3002;
 
-// Middleware for parsing JSON and urlencoded from data, and route. 
-
+// Middleware for parsing JSON and urlencoded from data, and route. added static middleware for serving assets in public folder
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
